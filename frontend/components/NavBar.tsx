1a01301285521a2d51c9d02dc13fd4476e018d3a
Quinta <0pietroquintavalle0@gmail.com>
2024-07-01 02:01:10 +0200

    Little stuff

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -111,7 +111,7 @@ const NavBar: React.FC = () => {
             Dashboard
           </Link>
           {!isLoggedIn ? (
-            <Link href="/login" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+            <Link href="/login" className="text-sm font-medium hover:underline underline-offset-4 border-2 px-4 py-2 rounded-full" prefetch={false}>
               Login
             </Link>
           ) : (
